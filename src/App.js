d9aa183b8bf58cf65477748d1b13ab65c1c089ba
심재호 <neverwinter@amorepacific.com>
2020-07-23 18:13:45 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import TodoTemaplte from 'components/TodoTemplate';
 import TodoInsert from 'components/TodoInsert';
 import TodoList from 'components/TodoList';
@@ -6,37 +6,25 @@ import _ from 'lodash';
 
 const App = () => {
 
-  const [todos, setTodos] = useState([
-    // {
-    //   id: 1,
-    //   text: '리액트의 기초 알아보기',
-    //   checked: true
-    // },
-    // {
-    //   id: 2,
-    //   text: '컴포넌트 스타일링해 보기',
-    //   checked: false
-    // },
-    // {
-    //   id: 3,
-    //   text: '일정 관리 앱 만들어 보기',
-    //   checked: false
-    // }
-  ]);
+  const [todos, setTodos] = useState([]);
 
   const insertTodo = useCallback(text => {
     const item = _.find(todos, item => item.text === text);
     if (item) {
       alert('이미 추가된 항목입니다.');
     } else {
-      const nextId = todos.length > 0 ? todos[todos.length - 1] + 1 : 1;    
+      const nextId = todos.length > 0 ? todos[todos.length - 1].id + 1 : 1;    
       const todo = {
         id: nextId,
         text: text,
         checked: false
-      };        
+      };              
       setTodos(todos.concat(todo));
     }    
+  }, [todos]);
+
+  useEffect(() => {
+    console.log(todos);
   }, [todos]);
 
   const changeChecked = useCallback(todo => {    
